7cb8d2cdd2f82a3d814b88ef136d09bbfaee24b6
KRPLAB <kauan.r.paulino@gmail.com>
2025-11-22 15:58:21 -0300

    :recycle: refactor: importando prisma 6

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,6 +1,5 @@
-import { PrismaClient } from '../generated/client';
+import { PrismaClient } from "@prisma/client"
 
 export const prisma = new PrismaClient({
-  accelerateUrl: '',
   log: ['query', 'info', 'warn', 'error'],
 });
